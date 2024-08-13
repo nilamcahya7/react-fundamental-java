6fa4a92bd764dfef6bda0fed77436cd58e3674a3
Nilam <nilyacahya4@gmail.com>
2024-08-13 09:33:17 +0700

    add styling for login page

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -7,7 +7,7 @@ let Dashboard = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100 text-center">
       <h1>Login Success</h1>
-      <h3>Welcome to the Dashboard, {username}!</h3>
+      <h3>Welcome to the Dashboard, <span>{username}!</span></h3>
       <a href="@{/account/password/change}" class="mt-3"><h5>Change Password</h5></a>
       <a href="@{/account/role}" class="link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Role Management</a>
     </div>
